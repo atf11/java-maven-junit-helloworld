14dc80b4e166f1ede3b796a0c18c4b285ae55751
xuyab <yabin.xu@hp.com>
2015-10-20 13:49:24 +0800

    中文测试

--- testHello/testHelloworld/src/testHelloworld/HelloWorld.java
+++ testHello/testHelloworld/src/testHelloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package testHelloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub test01
+		// TODO Auto-generated method stub ÖĞÎÄ²âÊÔ
 		System.out.println("Hello, World");
 
 	}
